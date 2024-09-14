b43c7ff3806fbda4685aaefe1db250822348c79a
Ciro Diaz <cirod307@gmail.com>
2024-09-14 19:18:07 +0200

    Javadoc

--- src/main/java/net/opencraft/renderer/texture/Assets.java
+++ src/main/java/net/opencraft/renderer/texture/Assets.java
@@ -33,22 +33,57 @@ public class Assets {
 		return create(tilesheets.toArray(new Tilesheet[0]));
 	}
 	
+	/**
+	 * This method returns a button.
+	 * 
+	 * Do not call this method directly! This method may use too much
+	 * CPU. Instead, we recommend saving it to a {@link java.awt.image.BufferedImage}
+	 * and retrieve it whenever.
+	 */
 	public BufferedImage getButton(int button_id) {
 		return gui.get(0, button_id * 20, 200, 20);
 	}
 
+	/**
+	 * This method returns an arrow.
+	 * 
+	 * Do not call this method directly! This method may use too much
+	 * CPU. Instead, we recommend saving it to a {@link java.awt.image.BufferedImage}
+	 * and retrieve it whenever.
+	 */
 	public BufferedImage getArrow(int arrow) {
 		return gui.get(200 + 15 * arrow, 20, 14, 22);
 	}
 
+	/**
+	 * This method returns the Minecraft Logo.
+	 * 
+	 * Do not call this method directly! This method may use too much
+	 * CPU. Instead, we recommend saving it to a {@link java.awt.image.BufferedImage}
+	 * and retrieve it whenever.
+	 */
 	public BufferedImage getLogo() {
 		return gui.get(0, 61, 271, 44);
 	}
 
+	/**
+	 * This method returns a dirt block but darker.
+	 * 
+	 * Do not call this method directly! This method may use too much
+	 * CPU. Instead, we recommend saving it to a {@link java.awt.image.BufferedImage}
+	 * and retrieve it whenever.
+	 */
 	public BufferedImage getBackground() {
 		return gui.get(242, 0, 16, 16);
 	}
 	
+	/**
+	 * This method returns the OpenCraft HAECATOMBE logo.
+	 * 
+	 * Do not call this method directly! This method may use too much
+	 * CPU. Instead, we recommend saving it to a {@link java.awt.image.BufferedImage}
+	 * and retrieve it whenever.
+	 */
 	public BufferedImage getLoadingLogo() {
 		return loadingLogo.get().getImage();
 	}
